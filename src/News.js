3c0757d963194046f7fc57dfbdefd47983525d68
veerababu pandrakula <105121433+Veerababupandrakula@users.noreply.github.com>
2024-01-14 10:35:02 +0530

    first commit

--- src/News.js
+++ src/News.js
@@ -1,13 +1,14 @@
 export default function News(props){
     return (
         <div className="grid-container">
-        <div className="grid-item">
-            <img src={props.article.urlToImage} alt={props.title}/>
+        <div> 
             <h3>{props.article.title}</h3>
+            <img src={props.article.urlToImage} alt={"news Image"} width={400}/>
             <p>{props.article.description?.substring(0,100).concat("......") 
             } <a href={props.article.url} target="blank">read more</a></p> 
         </div>
         </div>
+        
         
     )
 }
